cfdf5715b138c8da2c9d79ab7311ba34467ef8d1
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-13 12:39:43 -0300

    Arquivos das aulas 10 e 11 de python adicionados

---
+++ Python/Aula10.py
@@ -0,0 +1,28 @@
+from datetime import date, time, datetime, timedelta
+
+
+def mostra_date():
+    hoje = date.today()
+    print(hoje.strftime('%d/%m/%Y'))
+
+
+def mostra_time():
+    hora = time(hour=12, minute=43, second=45)
+    print(hora.strftime('%H:%M:%S'))
+
+
+def mostra_date_time():
+    date_time = datetime.now()
+    print(date_time.strftime('%d/%m/%Y %H:%M:%S'))
+
+
+def operacoes_date():
+    data = date(1992, 6, 29)
+    diferenca = data + timedelta(days=365)
+    print(diferenca)
+
+***REMOVED***
+    mostra_date()
+    mostra_time()
+    mostra_date_time()
+    operacoes_date()
---
+++ Python/Aula11.py
@@ -0,0 +1,48 @@
+def exception():
+    lista = [1, 10]
+    arquivo = open('teste.txt', 'w')
+    try:
+        divisao = 10 / 1
+        numero = lista[1]
+        texto = arquivo.read()
+        x = a = 5
+    except ZeroDivisionError:
+        print("Não é possivél dividir por zero")
+    except IndexError:
+        print("Indice inexistente na lista")
+    except Exception as ex:
+        print(ex)
+    else:
+        print("Nã ocorreu Exception")
+    finally:
+        print('Arquivo fechado')
+        arquivo.close()
+        print("Sempre executa")
+
+
+def personalizacao():
+    while True:
+        try:
+            x = int(input("Entre com uma nota de 0 a 10\n"))
+            if x > 10:
+                raise InputError('Não pode ser maior qeu 10')
+            elif x < 0:
+                raise InputError('Não pode ser menor qeu 0')
+            break
+        except ValueError:
+            print("Valor invalido. Apenas inteiros entre 0 e 10 são permitidos")
+        except InputError as ex:
+            print(ex)
+
+
+class Error(Exception):
+    pass
+
+
+class InputError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+***REMOVED***
+    personalizacao()
