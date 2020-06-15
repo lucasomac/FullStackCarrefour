e3487082ac4c584f194def44d719e1957912ed09
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-15 19:46:48 -0300

    Adicionado arquivos do curso de introdução de JavaScript

---
+++ JavaScript/introducao/js/main.js
@@ -0,0 +1,26 @@
+// var nome = "Lucas Macedo"
+// alert("Bem vindo" + nome)
+
+function clicou() {
+    alert("Clicou no botão")
+}
+
+function redirecionar() {
+    window.open('https://facebook.com')
+}
+
+function trocar(element) {
+    element.innerHTML = 'Obrigado por passar o mouse';
+}
+
+function voltar(element) {
+    element.innerHTML = 'Passe o mouse aqui ';
+}
+
+function load() {
+    alert("Acessou a pagina")
+}
+
+function funcaoChange(element) {
+    alert(element.value)
+}
