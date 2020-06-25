f92359f02d6b7e52caf72667831c44e30cf2cf3e
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-25 17:36:05 -0300

    Adicionado arquivos da aula de teste do Javascript

---
+++ JavaScript/testes/src/math.js
@@ -0,0 +1,20 @@
+// acrescentamos o método printSum
+
+// math.js
+class Math {
+  sum = function sum(a, b, callback) {
+	setTimeout(() => {
+		callback(a + b);
+	}, 2500);
+  }
+
+  multiply = function multiply(a, b) {
+    return a * b;
+  }
+
+  printSum(req, res, a, b) {
+    res.load('index', a + b);
+  }
+}
+
+module.exports = Math;
